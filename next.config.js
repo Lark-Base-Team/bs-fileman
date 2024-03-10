8d48f8bfa56fe900b63843c626ec8ef5b26fc42c
周寰 <zhouhuan@zhouhuans-Mac-mini.local>
2024-03-10 15:55:41 +0800

    modify next config

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@ const { i18n } = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n,
+  // i18n,
+  output: 'export',
+  distDir: 'dist',
+  assetPrefix: './',
+
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
-  transpilePackages: [
-    "@douyinfe/semi-ui",
-    "@douyinfe/semi-icons",
-    "@douyinfe/semi-illustrations",
-  ],
-};
+  transpilePackages: ['@douyinfe/semi-ui', '@douyinfe/semi-icons', '@douyinfe/semi-illustrations'],
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
